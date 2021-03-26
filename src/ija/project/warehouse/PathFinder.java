90f27c7f14006badaf8535a7c75b53dc08465cb4
Tomas Beranek <xberan46@stud.fit.vubr.cz>
2021-03-26 11:37:40 +0100

    add findPath method stump

--- src/ija/project/warehouse/PathFinder.java
+++ src/ija/project/warehouse/PathFinder.java
@@ -17,7 +17,33 @@ public class PathFinder {
           List<Pair<String, Integer>> order,
           Hashtable<Integer, ShelfRectangle> shelfs) {
       // calculates the optimal path for a cart
-      return null;
+
+      // DELETE: just a stump for WarehouseSimulation class development
+      List<Pair<Integer, Pair<String, Integer>>> path = new ArrayList<>();
+      Pair<String, Integer> doNotPickUp = new Pair<>("", 0);
+      Pair<String, Integer> goodsToPickUp = new Pair<>("Šroub inbus půlkulatý s límcem M24 nerez A2", 50);
+
+      for (int i = 0; i <= 22 ; i++) {
+        path.add(new Pair<Integer, Pair<String, Integer>>(i, doNotPickUp));
+      }
+      path.add(new Pair<Integer, Pair<String, Integer>>(45, doNotPickUp));
+      path.add(new Pair<Integer, Pair<String, Integer>>(68, doNotPickUp));
+      path.add(new Pair<Integer, Pair<String, Integer>>(91, doNotPickUp));
+      path.add(new Pair<Integer, Pair<String, Integer>>(114, doNotPickUp));
+      // pick up goods
+      path.add(new Pair<Integer, Pair<String, Integer>>(113, goodsToPickUp));
+
+      for (int i = 112; i >= 92; i--) {
+        path.add(new Pair<Integer, Pair<String, Integer>>(i, doNotPickUp));
+      }
+
+      path.add(new Pair<Integer, Pair<String, Integer>>(69, doNotPickUp));
+      path.add(new Pair<Integer, Pair<String, Integer>>(46, doNotPickUp));
+      path.add(new Pair<Integer, Pair<String, Integer>>(23, doNotPickUp));
+      // ends in dispensing point
+      path.add(new Pair<Integer, Pair<String, Integer>>(0, doNotPickUp));
+
+      return path;
   }
 
 
